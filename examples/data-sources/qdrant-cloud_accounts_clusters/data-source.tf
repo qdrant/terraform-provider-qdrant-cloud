cd93567443d185b8feed7a369b1c18664cd86e11
Mikheil Lomidze <mikheil.lomidze@qdrant.com>
2024-09-16 17:54:01 +0400

    Change examples

--- examples/data-sources/qdrant-cloud_accounts_clusters/data-source.tf
+++ examples/data-sources/qdrant-cloud_accounts_clusters/data-source.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = ">= 1.7.0"
   required_providers {
     qdrant-cloud = {
-      source  = "local/qdrant-cloud/qdrant-cloud"
-      version = ">=1.0"
+      source  = "qdrant/qdrant-cloud"
+      version = ">=1.1.0"
     }
   }
 }
@@ -12,15 +12,14 @@ terraform {
 // Add the provider to specify some provider wide settings
 provider "qdrant-cloud" {
   api_key    = "" // API Key generated in Qdrant Cloud (required)
-  api_url    = "" // URL where the public API of Qdrant cloud can be found (can be left empty if the production URL need to be used)
   account_id = "" // The default account ID you want to use in Qdrant Cloud (can be overriden on resource level)
 }
 
-data "qdrant-cloud_accounts_clusters" "test" {
+
+data "qdrant-cloud_accounts_clusters" "all_clusters" {
   // No keys needed here, the account ID is specified on provider level
 }
 
 output "clusters" {
-  value = data.qdrant-cloud_accounts_clusters.test.clusters
+  value = data.qdrant-cloud_accounts_clusters.all_clusters.clusters
 }
-
--- examples/data-sources/qdrant-cloud_booking_packages/data-source.tf
+++ examples/data-sources/qdrant-cloud_booking_packages/data-source.tf
@@ -3,8 +3,8 @@ terraform {
   required_version = ">= 1.7.0"
   required_providers {
     qdrant-cloud = {
-      source  = "local/qdrant-cloud/qdrant-cloud"
-      version = ">=1.0"
+      source  = "qdrant/qdrant-cloud"
+      version = ">=1.1.0"
     }
   }
 }
@@ -12,17 +12,16 @@ terraform {
 // Add the provider to specify some provider wide settings
 provider "qdrant-cloud" {
   api_key    = "" // API Key generated in Qdrant Cloud (required)
-  api_url    = "" // URL where the public API of Qdrant cloud can be found (can be left empty if the production URL need to be used)
   account_id = "" // The default account ID you want to use in Qdrant Cloud (can be overriden on resource level)
 }
 
 // Reference the booking packages
 data "qdrant-cloud_booking_packages" "test" {
-  // No filter here
+  cloud_provider = "aws"  // Required. Please refer to the documentation (https://registry.terraform.io/providers/qdrant/qdrant-cloud/latest/docs/guides/getting-started) for the available options.
+  cloud_region   = "us-west-2"  // Required. Please refer to the documentation (https://registry.terraform.io/providers/qdrant/qdrant-cloud/latest/docs/guides/getting-started) for the available options.
 }
 
 // Output the packages (Note that the package id is relevent for creating a cluster)
 output "packages" {
   value = data.qdrant-cloud_booking_packages.test.packages
 }
-
